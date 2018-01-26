ec6191d6be702f1ab6270f7b263b2107125c7244
root <root@ose3x-master.example.com>
2018-01-26 13:23:00 -0800

    update

--- build_proxy.sh
+++ build_proxy.sh
@@ -6,10 +6,8 @@ function setup_proxy(){
 
 function build_istio_proxy(){
 
-  mkdir -p /root/project
-  cp -rf /root/proxy/src /root/project/
-
   cd /root/proxy
+  make cmake-x86 CMAKE_MAKE_OPT="-j 8"
   make cmake-x86 CMAKE_MAKE_OPT="-j 8"
 
   TAG=TB12 script/release-docker-rhel debug
