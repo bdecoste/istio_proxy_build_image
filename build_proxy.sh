210e11f8f610fa5a4ef642b8436087da6a641581
root <root@ose3x-master.example.com>
2018-01-26 13:24:53 -0800

    update

--- build_proxy.sh
+++ build_proxy.sh
@@ -6,6 +6,9 @@ function setup_proxy(){
 
 function build_istio_proxy(){
 
+  mkdir /root/proxy/project
+  cp -rf /root/proxy/src /root/proxy/project
+
   cd /root/proxy
   make cmake-x86 CMAKE_MAKE_OPT="-j 8"
   make cmake-x86 CMAKE_MAKE_OPT="-j 8"
